c75caf7126d762c6dfee34f82f686b19360d5c93
Rafael Carvallo <rafael.carvallo@outlook.com>
2024-12-04 14:56:53 -0300

    changed setup to include missing packages

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='cpe_manager',
-      version='0.0.2',
+      version='0.1.0',
       description='Tools for CPE management (mostly VSOL/Chinese ONUs)',
       author='Rafael Carvallo',
       author_email='rafael.carvalloh@gmail.com',
       url='',
-      packages=['cpe_manager', 'cpe_manager.models'],
+      packages=['cpe_manager', 'cpe_manager.models', 'cpe_manager.models.vsol'],
       install_requires=[
        'requests',
        'beautifulsoup4',
